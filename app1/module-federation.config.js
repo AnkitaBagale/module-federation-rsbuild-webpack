0d4f732f04bcdbabfb9a96cf8d3fde41aa825841
AnkitaBagale <ankita.bagale@razorpay.com>
2024-11-05 18:22:32 +0530

    particular-version

--- app1/module-federation.config.js
+++ app1/module-federation.config.js
@@ -3,7 +3,6 @@ export const mfConfig = {
   filename: "app1.remoteEntry.js",
   remotes: {
     app2: "app2@http://localhost:3000/app2.remoteEntry.js",
-    // x: 'x@https://localhost:8080/dist/x.remoteEntry.js'
   },
   // exposes: {
   //   "./Tab": "./src/Tab",
