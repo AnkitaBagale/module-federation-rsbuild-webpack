9c7d521b4f7db77be43f134a3e7c48c45479bdfc
AnkitaBagale <ankita.bagale@razorpay.com>
2024-11-06 10:14:22 +0530

    working rsbuild with x remote

--- app1/src/App.tsx
+++ app1/src/App.tsx
@@ -1,19 +1,25 @@
 import React from "react";
-const RemoteButton = React.lazy(() =>
-  // @ts-expect-error Import from remote which is a federated module
-  import("app2/Button").then((module) => ({
-    default: module.Button || module.default,
-  }))
-);
+// const RemoteButton = React.lazy(() =>
+//   // @ts-expect-error Import from remote which is a federated module
+//   import("app2/Button").then((module) => ({
+//     default: module.Button || module.default,
+//   }))
+// );
+
+//@ts-expect-error Import from remote which is a federated module
+import { Pill } from "x/ui";
 
 const App = () => {
   return (
     <div className="content">
       <h1>App1</h1>
-      <React.Suspense fallback="loading button..">
-        {/*  @ts-expect-error Import from remote which is a federated module */}
-        <RemoteButton>Button: federated module from App2</RemoteButton>
+      <React.Suspense>
+        <Pill type='success'>App1 Pill</Pill>
       </React.Suspense>
+      {/* <React.Suspense fallback="loading button.."> */}
+      {/* // @ts-expect-error Import from remote which is a federated module */}
+      {/* <RemoteButton>Button: federated module from App2</RemoteButton> */}
+      {/* </React.Suspense> */}
     </div>
   );
 };
